b13e4629f79506a648ca4376c2910b199a9f5c7f
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-01 18:33:22 -0500

    removed testing message

--- scan.py
+++ scan.py
@@ -2,14 +2,12 @@ import pytesseract
 import cv2
 import sys
 
-print("got here")
-
 try:
     filename = "uploads/" + sys.argv[1]
 
     img = cv2.imread("uploads/testimage.png")
     text = pytesseract.image_to_string(img)
 
-    print(text)
+    print("Your text: " + text)
 except:
     print("There was an error running this program!")
