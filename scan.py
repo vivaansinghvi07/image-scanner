f14e932e888f13cec5982123777b321021062b4c
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-01 18:35:56 -0500

    fixes(again)

--- scan.py
+++ scan.py
@@ -5,9 +5,9 @@ import sys
 try:
     filename = "uploads/" + sys.argv[1]
 
-    img = cv2.imread("uploads/" + filename)
+    img = cv2.imread(filename)
     text = pytesseract.image_to_string(img)
 
-    print("Your text: " + text)
+    print(text)
 except:
     print("There was an error running this program!")
