4fd8c5472e27aafea8f21941887a8041f83ca0fc
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-01 18:35:02 -0500

    fixes

--- scan.py
+++ scan.py
@@ -5,7 +5,7 @@ import sys
 try:
     filename = "uploads/" + sys.argv[1]
 
-    img = cv2.imread("uploads/testimage.png")
+    img = cv2.imread("uploads/" + filename)
     text = pytesseract.image_to_string(img)
 
     print("Your text: " + text)
