71f3c0aa889f465c5c687ceacc7c6c3827fd466c
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-16 15:52:45 -0400

    added translating feature

--- scan.py
+++ scan.py
@@ -1,13 +1,31 @@
+# imports libraries
 import pytesseract
 import cv2
 import sys
+from langdetect import detect 
+from translate import Translator
+
 
 try:
+    # gets the name of the file being looked at
     filename = "uploads/" + sys.argv[1]
 
+    # loads the image
     img = cv2.imread(filename)
+
+    # converts the image text to a string
     text = pytesseract.image_to_string(img)
 
-    print(text)
+    # detects the language
+    lang = detect(text)
+
+    # creates the translation
+    translator = Translator(from_lang=lang, to_lang='en')
+    trans = translator.translate(text)
+
+    # prints outputs
+    print("Text: " + text)
+    print("<br>Translated: " + trans)
+
 except:
     print("There was an error running this program!")
