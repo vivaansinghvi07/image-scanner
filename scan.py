bbe5c8c07bd460c2936c7c781645bbb1f65cea5e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-26 18:21:09 -0400

    removed translator (buggy)

--- scan.py
+++ scan.py
@@ -2,9 +2,6 @@
 import pytesseract
 import cv2
 import sys
-from langdetect import detect 
-from translate import Translator
-
 
 try:
     # gets the name of the file being looked at
@@ -16,16 +13,8 @@ try:
     # converts the image text to a string
     text = pytesseract.image_to_string(img)
 
-    # detects the language
-    lang = detect(text)
-
-    # creates the translation
-    translator = Translator(from_lang=lang, to_lang='en')
-    trans = translator.translate(text)
-
     # prints outputs
-    print("Text: " + text)
-    print("<br>Translated: " + trans)
+    print(text)
 
 except:
     print("There was an error running this program!")
